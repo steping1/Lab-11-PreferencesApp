ba5872829f1b2c74f09444d12d7beabd69cdd1be
steping1 <mehmet125wtf@gmail.com>
2025-12-25 21:28:40 +0300

    feat: global auth state using context api

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'; 
 import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
+import { AuthProvider } from './context/AuthContext';
 import LoginScreen from './screens/LoginScreen'; 
 import HomeScreen from './screens/HomeScreen'; 
 import SettingsScreen from './screens/SettingsScreen'; 
@@ -8,12 +9,14 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthProvider> 
+      <NavigationContainer> 
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Settings" component={SettingsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthProvider>
   );
-} 
+}
